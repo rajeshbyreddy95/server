2307d0afe34703213f630c3bc427fdc586dadfdc
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-05-11 00:04:51 +0530

    updated auth router

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5050;
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 
