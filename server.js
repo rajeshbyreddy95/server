1711b63e4320c7877588e675759e72f5c16dbbec
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-05-11 00:02:20 +0530

    updated auth router

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const cors = require('cors');
-
+const authRoutes = require('./routes/auth');
 
 
 require('dotenv').config();
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.json())
 
 
-
+app.use('/api/auth', authRoutes);
 
 app.get('/', async (req, res) => {
   res.send('Hello World from Node.js Server!');
